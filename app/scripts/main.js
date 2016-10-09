8437b833023d05586da51fcf0dc15c6a9b2d4f2b
Eric Sun <ericsun977@gmail.com>
2016-10-09 19:52:39 -0400

    init angular module and ng-route

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,13 +2,32 @@
   'use strict';
 
   angular
-    .module('equilibrium', [])
+    .module('equilibrium', [
+      'ngRoute'
+    ])
     .config(Config);
 
-  Config.$inject = ['$locationProvider'];
+  Config.$inject = [
+    '$locationProvider',
+    '$routeProvider'
+  ];
 
-  function Config($locationProvider) {
+  function Config(
+    $locationProvider,
+    $routeProvider) {
+
+
     $locationProvider.html5Mode(true);
+
+    $routeProvider
+      .when('/', {
+        pageType: 'home',
+        templateUrl: '../views/home/home-view.html'
+      })
+      .when('/team', {
+        pageType: 'team',
+        templateUrl: '../views/team/team-view.html'
+      });
   }
 
 })();
